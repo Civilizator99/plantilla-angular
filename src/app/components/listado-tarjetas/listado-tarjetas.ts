5e828cdb84dcae879367b67cc2e47e56d656a48b
Civilizator99 <tigervespamonx5@gmail.com>
2025-06-05 13:21:42 +0200

    added stuff 05/06/25 3

--- src/app/components/listado-tarjetas/listado-tarjetas.ts
+++ src/app/components/listado-tarjetas/listado-tarjetas.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IdolService } from '../../services/idol.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-listado-tarjetas',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './listado-tarjetas.html',
   styleUrl: './listado-tarjetas.css'
 })
-export class ListadoTarjetas {
+export class ListadoTarjetas implements OnInit {
+  public idols: Idol[];
 
+
+  constructor(public idolService: IdolService){
+  this.idols = [];
 }
+  ngOnInit(): void {
+    //LLamada al servicio, se debe hacer siempre desde el método ng OnInit para no hacerlo desde el constructor
+    this.idols = this.idolService.getIdols();
+  }
+}
+
+
--- src/app/services/idol.service.ts
+++ src/app/services/idol.service.ts
@@ -12,10 +12,10 @@ export class IdolService {
 
 
   // Métodos
-public getIdols(): Idol[]{
-  const listadoIdols: Idol[] = [{
+  public getIdols(): Idol[] {
+    const listadoIdols: Idol[] = [{
       nombre: "Sasshi",
-      imagen: "img/sasshi1.jpg",
+      imagen: "img/sasshi1.png",
       honor: 98,
       ataque: 45,
       defensa: 93,
@@ -24,7 +24,7 @@ public getIdols(): Idol[]{
     },
     {
       nombre: "Acchan",
-      imagen: "",
+      imagen: "img/acchan1.png",
       honor: 92,
       ataque: 83,
       defensa: 63,
@@ -32,8 +32,8 @@ public getIdols(): Idol[]{
       vida: 26
     },
     {
-      nombre: "Harugon",
-      imagen: "img/harugon1.jpg",
+      nombre: "Mayuyu",
+      imagen: "img/mayu1.png",
       honor: 60,
       ataque: 75,
       defensa: 73,
@@ -42,29 +42,35 @@ public getIdols(): Idol[]{
     },
     {
       nombre: "Kojiharu",
-      imagen: "img/kojiharu1.jpg",
+      imagen: "img/kojiharu1.png",
       honor: 80,
-      ataque: 120,
+      ataque: 100,
       defensa: 50,
       poder: 550,
       vida: 100
     },
     {
       nombre: "Erii",
-      imagen: "img/erii1.jpg",
+      imagen: "img/erii1.png",
       honor: 60,
       ataque: 75,
-      defensa: 73,
-      poder: 450,
+      defensa: 93,
+      poder: 280,
       vida: 52
     },
-    // this.listadoIdols = []; //dejar lista vacía
+    {
+      nombre: "Tomo",
+      imagen: "img/tomomi1.png",
+      honor: 80,
+      ataque: 85,
+      defensa: 63,
+      poder: 550,
+      vida: 72
+    },
+      // this.listadoIdols = []; //dejar lista vacía
     ];
     return listadoIdols;
-}
+  }
 
-public getSasshi(){
-
-}
 
 }
